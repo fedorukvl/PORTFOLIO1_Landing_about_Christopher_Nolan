2742abbfd2f1eb8d6a20fdf6f81f833bdfa55b3f
Vasiliy <fedorukvl27@gmail.com>
2020-08-21 04:50:50 +0300

    Updates

---
+++ precache-manifest.14a866a68065417b574c6e59c4436150.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73c1642e6979ea70724a99e053ab28f",
+    "url": "/PORTFOLIO1_Landing_about_Christopher_Nolan/index.html"
+  },
+  {
+    "revision": "37a457f7dfbe758b76d4",
+    "url": "/PORTFOLIO1_Landing_about_Christopher_Nolan/static/css/main.b50a7018.chunk.css"
+  },
+  {
+    "revision": "e2d3c1107a6a43259ec9",
+    "url": "/PORTFOLIO1_Landing_about_Christopher_Nolan/static/js/2.aee7c933.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PORTFOLIO1_Landing_about_Christopher_Nolan/static/js/2.aee7c933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a457f7dfbe758b76d4",
+    "url": "/PORTFOLIO1_Landing_about_Christopher_Nolan/static/js/main.5547aebb.chunk.js"
+  },
+  {
+    "revision": "3193cccbdca57a0b32f0",
+    "url": "/PORTFOLIO1_Landing_about_Christopher_Nolan/static/js/runtime-main.5f04a9f8.js"
+  },
+  {
+    "revision": "f7f02d6ce5eb16b89b89a548d5810b21",
+    "url": "/PORTFOLIO1_Landing_about_Christopher_Nolan/static/media/1.f7f02d6c.jpg"
+  },
+  {
+    "revision": "eed7ccd9f5658e670e6233c5881f30e3",
+    "url": "/PORTFOLIO1_Landing_about_Christopher_Nolan/static/media/interstellar.eed7ccd9.jpg"
+  },
+  {
+    "revision": "256fb30ddfc59947981f56caa054e434",
+    "url": "/PORTFOLIO1_Landing_about_Christopher_Nolan/static/media/memento.256fb30d.jpg"
+  },
+  {
+    "revision": "ca3b39a351ef7b2c97d52633f5554794",
+    "url": "/PORTFOLIO1_Landing_about_Christopher_Nolan/static/media/nolan1.ca3b39a3.jpg"
+  },
+  {
+    "revision": "3f1d0a2112a7565298aafc083b9121d1",
+    "url": "/PORTFOLIO1_Landing_about_Christopher_Nolan/static/media/nolan3.3f1d0a21.jpg"
+  },
+  {
+    "revision": "79e45c7ef8424eb6e479fbe5958a45b3",
+    "url": "/PORTFOLIO1_Landing_about_Christopher_Nolan/static/media/nolan4.79e45c7e.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/PORTFOLIO1_Landing_about_Christopher_Nolan/precache-manifest.14a866a68065417b574c6e59c4436150.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PORTFOLIO1_Landing_about_Christopher_Nolan/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
